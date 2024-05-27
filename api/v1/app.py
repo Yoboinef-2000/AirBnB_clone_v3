9d61b676433abbc1f7e70b5461647acb1ffd334e
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-28 01:28:55 +0300

    Documentation issues.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""The Flask WeB Application."""
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,7 +18,8 @@ def tearItAllUp(error):
 
 @app.errorhandler(404)
 def weNotHereBabyWeOut(error):
-    return jsonify({"error": "Not found"}), 404
+    # return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
